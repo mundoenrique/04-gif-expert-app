fe6801807a28bb3e07ddd5bcbd1664eae00c7ee0
epenaloza <epenaloza@novopayment.com>
2022-03-11 17:45:52 -0400

    Crea y agrega catorias ramdom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [categories, setCategories] = useState(['One Punch', 'Samurai x', 'Dragon Ball']);
+
+	const handleAddCat = () => {
+		const categoryNew = (Math.random() + 1).toString(36).substring(2);
+		setCategories([categoryNew, ...categories]);
+		//otra forma
+		// setCategories(cats => [categoryNew, ...cats]);
+	}
+
+	return (
+		<>
+			<h2>GifExpertApp</h2>
+			<hr />
+			<button onClick={handleAddCat}>Agregar</button>
+			<ol>
+				{
+					categories.map(category => <li key={category}>{category}</li>)
+				}
+			</ol>
+		</>
+	);
 }
 
 export default App;
