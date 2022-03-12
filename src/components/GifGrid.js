76e4083a250dcce7497d5ad184ac7a77fe55e342
epenaloza <epenaloza@novopayment.com>
2022-03-12 10:42:20 -0400

    Crea hooks personalizado

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,15 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from 'react';
 
-import GifGridItem from "./GifGridItem";
-import { getGifs } from "./helpers/helperTools";
+import GifGridItem from './GifGridItem';
+import useFetchGifts from '../hooks/useFetchGifts';
 
 export default function GifGrid({ category }) {
-	const [images, setImages] = useState([])
-	useEffect(() => {
-		getGifs(category)
-			.then(setImages)
-		// es igual a esto
-		// .then(imgs => setImages(imgs))
-	}, [category])
+	const { data: images, loading } = useFetchGifts(category);
 
 	return (
 		<>
 			<h3>{category}</h3>
+			{loading && <p>Cargando...</p>}
 			<div className="card-grid">
 				{
 					images.map(img => (
