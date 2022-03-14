e504ec2e2ef3edba8da1044fc6b006395a31c9e4
mundoenrique <mundoenrique@hotmail.com>
2022-03-14 15:05:38 -0400

    Pruebas en el componente AddCategory

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -2,8 +2,13 @@ import { shallow } from "enzyme";
 import AddCategory from "../../components/AddCategory";
 
 describe('Pruebas para el componente <AddCategory />', () => {
-	const setCategories = () => { }
-	const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	const setCategories = jest.fn();
+	let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		wrapper = shallow(<AddCategory setCategories={setCategories} />);
+	});
 
 	test('Debe mostrarse correctamente', () => {
 
@@ -19,5 +24,27 @@ describe('Pruebas para el componente <AddCategory />', () => {
 		const inputValue = wrapper.find('input');
 
 		expect(inputValue.prop('value')).toBe(value)
+	});
+
+	test('No debe llamar al submit', () => {
+		wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+
+		expect(setCategories).not.toHaveBeenCalled();
+	});
+
+	test('Debe hacer el llamado al setCategories', () => {
+		const value = 'samurai x';
+
+		wrapper.find('input').simulate('change', { target: { value } });
+		wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+		expect(setCategories).toHaveBeenCalled();
+		expect(setCategories).toHaveBeenCalledTimes(1);
+		expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+
+		const inputValue = wrapper.find('input');
+
+		expect(inputValue.prop('value')).toBe('');
 	});
 });
